9c5d27f2ad2ef6e44bc8549e2d449b3b37aa98eb
wangyabin <1045875623@qq.com>
2025-05-28 01:06:31 +0800

    slidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,35 +6,44 @@ export default defineConfig({
   title: 'VitePress',
   description: 'Vite & Vue powered static site generator.',
   base: '/',
+
+
   themeConfig: {
-    nav: [
-      { text: 'Example', link: '/example' },
+      nav: [
+        { text: 'Example', link: '/example' },
 
-      {
-        text: '算法题库',
-        items: [
-          { text: '数组专题', link: '/algorithms/array' },
-          { text: '动态规划', link: '/algorithms/dp' },
-        ],
-      },
+        {
+          text: '算法题库',
+          items: [
+            { text: '数组专题', link: '/algorithms/array' },
+            { text: '动态规划', link: '/algorithms/dp' },
+          ],
+        },
 
-    ],
+      ],
 
-    sidebar: [
-      {
-        text: '算法题库',
-        items: [
-          { text: '数组专题', link: '/algorithms/array' },
-          { text: '动态规划', link: '/algorithms/dp' },
-        ],
-      },
-      {
-        text: 'NLP',
-        items: [
-          { text: 'NLP', link: '/nlp/cosine' },
-        ],
-      }
-    ],
+      sidebar: [
+        {
+          text: '算法题库',
+          collapsible: true,  // 允许折叠
+          collapsed: false,   // 默认展开
+          items: [
+            { text: '数组专题', link: '/algorithms/array' },
+            { text: '动态规划', link: '/algorithms/dp' },
+          ],
+        },
+        {
+          text: 'NLP',
+          collapsible: true,  // 允许折叠
+          collapsed: false,   // 默认展开
+          items: [
+            { text: 'NLP', link: '/nlp/cosine' },
+          ],
+        }
+      ]
 
   },
+
+
 });
+
